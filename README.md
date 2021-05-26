b1a5ef6fbab53846e0107397575bf7842357e77e
Ken <kwill1253@gmail.com>
2021-05-26 18:00:40 -0400

    initial

---
+++ README.md
@@ -0,0 +1,38 @@
+![](./six910.png)
+
+# Six-9-10 UI
+
+[![Build Status](https://travis-ci.org/Ulbora/Six910-ui.svg?branch=master)](https://travis-ci.org/Ulbora/Six910-ui)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Six910-ui&metric=alert_status)](https://sonarcloud.io/dashboard?id=Six910-ui)
+[![Go Report Card](https://goreportcard.com/badge/github.com/Ulbora/Six910-ui)](https://goreportcard.com/report/github.com/Ulbora/Six910-ui)
+
+## Docker
+https://hub.docker.com/r/ulboralabs/six910-ui
+
+
+### Project Status
+Six910 has released and currently running on multiple sites.
+
+### The Speeding Fast Shopping Cart
+An open source ecommerce platform server for building online shopping cart systems; written in golang. This is the user's storefront and admin panel.
+
+This project is the storefront UI that works with Six910 REST implementation of a hosted ecommerce solution. 
+
+1. Users of Six910 UI, together with Six910 are able to customize templates just like other hosted solutions.
+2. Templates can either be written in Golang templating or use a JavaScript framework like Angular or React.
+3. REST services expose all cart functionality.
+
+## Admin Dashboard
+
+![](./six910-ul-admin-dashboard.png)
+
+## Template Designer
+There is a template designer to make desiging templates much easier than it currently is with most hosted shopping cart solutions.
+
+1. Users can download there current template
+2. Modify the template using the user's store URL
+3. Upload the template back to the hosted site
+
+
+
+
---
+++ static/admin/js/custom.js
@@ -0,0 +1,217 @@
+jQuery(document).ready(function ($) {
+    $(".clickable-row").click(function () {
+        window.location = $(this).data("href");
+    });
+});
+
+
+// $('#thumbnail').on('change',function(){
+//     alert("test");
+//     var fileName = $(this).val();
+// })
+
+$(document).ready(function () {
+    $("#thumbnail").keyup(function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("thumbnailImg").src = imageName;
+    });
+});
+
+$(document).ready(function () {
+    $("#image1").keyup(function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("image1Img").src = imageName;
+    });
+});
+
+
+
+$(document).ready(function () {
+    $("#image2").keyup(function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("image2Img").src = imageName;
+    });
+});
+
+
+
+$(document).ready(function () {
+    $("#image3").keyup(function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("image3Img").src = imageName;
+    });
+});
+
+
+
+$(document).ready(function () {
+    $("#image4").keyup(function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("image4Img").src = imageName;
+    });
+});
+
+
+$(document).ready(function () {
+    $("#image").keyup(function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("imageImg").src = imageName;
+    });
+});
+
+
+// $(document).ready(function () {
+//     $("#image").load(function () {
+//         //alert( $(this).val());
+//         var imageName = $(this).val();
+//         document.getElementById("imageImg").src = imageName;
+//     });
+// });
+
+
+$(document).ready(function () {
+    $("#image").on('load', function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("imageImg").src = imageName;
+    });
+});
+
+$(document).ready(function () {
+    $("#logo").keyup(function () {
+        //alert( $(this).val());
+        var imageName = $(this).val();
+        document.getElementById("logoImg").src = imageName;
+    });
+});
+
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+//   function drawVisitorChart() {
+
+//     // Create the data table.
+//     var data = new google.visualization.DataTable();
+//     data.addColumn('string', 'Day');
+//     data.addColumn('number', 'Visitors');
+//     data.addRows([
+//       ['11/1', 30],
+//       ['11/2', 10],
+//       ['11/3', 10],
+//       ['11/4', 10],
+//       ['11/5', 20]
+//     ]);
+
+//     // Set chart options
+//     var options = {'title':'Visitor Count by Day',
+//                    'width':500,
+//                    'height':400};
+
+//     // Instantiate and draw our chart, passing in some options.
+//     var chart = new google.visualization.ColumnChart(document.getElementById('visitor_chart_div'));
+//     chart.draw(data, options);
+//   }
+
+function drawVisitorChart(dataval) {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Day');
+    data.addColumn('number', 'Visitors');
+    data.addRows(dataval);
+
+    // Set chart options
+    var options = {
+        'title': 'Visitor Count by Day',
+        'width': 500,
+        'height': 400
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.ColumnChart(document.getElementById('visitor_chart_div'));
+    chart.draw(data, options);
+}
+
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+//   function drawSalesChart() {
+
+//     // Create the data table.
+//     var data = new google.visualization.DataTable();
+//     data.addColumn('string', 'Day');
+//     data.addColumn('number', '$ Sales');
+//     data.addRows([
+//       ['11/1', 300.25],
+//       ['11/2', 100.15],
+//       ['11/3', 101.25],
+//       ['11/4', 1001.55],
+//       ['11/5', 201.55]
+//     ]);
+
+//     // Set chart options
+//     var options = {'title':'Sales by Day',
+//                    'width':500,
+//                    'height':400};
+
+//     // Instantiate and draw our chart, passing in some options.
+//     var chart = new google.visualization.ColumnChart(document.getElementById('sales_chart_div'));
+//     chart.draw(data, options);
+//   }
+
+function drawSalesChart(dataval) {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Day');
+    data.addColumn('number', '$ Sales');
+    data.addRows(dataval);
+
+    // Set chart options
+    var options = {
+        'title': 'Sales by Day',
+        'width': 500,
+        'height': 400
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.ColumnChart(document.getElementById('sales_chart_div'));
+    chart.draw(data, options);
+}
+
+var loadVisitors = function (data) {
+    google.charts.setOnLoadCallback(drawVisitorChart(data));
+}
+
+var loadSales = function (data) {
+    google.charts.setOnLoadCallback(drawSalesChart(data));
+}
+
+
+function checkPasswordMatch() {
+    var password = $("#password").val();
+    var confirmPassword = $("#password2").val();
+    if (password != confirmPassword) {
+        document.getElementById("CheckPasswordMatch").style.visibility = "visible";
+        $("#CheckPasswordMatch").html("Passwords does not match!");
+    }
+    else {
+        document.getElementById("CheckPasswordMatch").style.visibility = "hidden";
+    }
+}
+
+$(document).ready(function () {
+    $("#password").keyup(checkPasswordMatch);
+});
+$(document).ready(function () {
+    $("#password2").keyup(checkPasswordMatch);
+});
